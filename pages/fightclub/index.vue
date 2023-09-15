97bfc292fe555260fe96030a80693493f8cd7fe3
mick <mickheudre@gmail.com>
2023-09-15 23:02:37 +0200

    test mobile calendar

--- pages/fightclub/index.vue
+++ pages/fightclub/index.vue
@@ -5,7 +5,10 @@
                 <h4 class="capitalize">événements</h4>
             </template>
             <template #footer>
-                <UButton v-if="userStore.profile?.permissions['fightClub'].find(p => p == 'eventCreate') ?? false" icon="i-heroicons-plus" @click="createEvent">Proposer un événement</UButton>
+                <div class="flex justify-end">
+                    <UButton v-if="userStore.profile?.permissions['fightClub'].find(p => p == 'eventCreate') ?? false" icon="i-heroicons-plus" @click="createEvent">Ajouter un événement</UButton>
+                    
+                </div>
             </template>
             <EventList @showDetails="showDetails" @edit-event="editEvent" @delete-event="deleteEvent"/>
             
@@ -21,26 +24,26 @@
             :time-from="9 * 60"
             :time-to="24 * 60"
             :disable-views="['years', 'year', 'month', 'day']">
-
-        <template #event="{ event, view }">
             
-            <div class="vuecal__event-title my-2 md:mx-2 text-white flex flex-col items-center justify-center" >
-                <span>{{ event.title }}</span>
+            <template #event="{ event, view }">
                 
-            </div>
-            
-            <small class="vuecal__event-time text-white">
-                <span>{{ `${event.start.formatTime()}→${event.end.formatTime()}` }}</span>
-            </small>
-
-            <div class="flex flex-col justify-end items-center h-auto">
-                <UTooltip class="m-2" v-if="event.organizer" :text="event.organizer.username" >
-                    <UAvatar :alt="event.organizer.username" size="sm" />
-                </UTooltip>
-                <!-- <UButton v-if="event.class=='opening_hour'" @click="" variant="link">Réserver une table</UButton> -->
-            </div>
-
-        </template>
+                <div class="vuecal__event-title my-2 md:mx-2 text-white flex flex-col items-center justify-center" >
+                    <span>{{ event.title }}</span>
+                    
+                </div>
+                
+                <small class="vuecal__event-time text-white">
+                    <div class="flex flex-col md:flex-row justify-center items-center"><span>{{ event.start.formatTime() }}</span><span>→</span><span>{{ event.end.formatTime() }}</span></div>
+                </small>
+                
+                <div class="flex flex-col justify-end items-center h-auto">
+                    <UTooltip class="m-2" v-if="event.organizer" :text="event.organizer.username" >
+                        <UAvatar :alt="event.organizer.username" size="sm" />
+                    </UTooltip>
+                    <!-- <UButton v-if="event.class=='opening_hour'" @click="" variant="link">Réserver une table</UButton> -->
+                </div>
+                
+            </template>
         </vue-cal>
         <vue-cal v-if="!loading && userCanManageEvent"
         locale="fr"
@@ -51,11 +54,12 @@
         :snap-to-time="15"
         :disable-views="['years', 'year', 'month', 'day']"
         :editable-events="{ title: false, drag: false, resize: false, delete: false, create: true }"
-        :drag-to-create-threshold="0"
+        :drag-to-create-threshold="15"
         @event-create="onEventCreateStart"
         @event-drag-create="onEventCreate"
         :on-event-click="onEventClick">
-
+        
+        
         <template #event="{ event, view }">
             
             <div class="vuecal__event-title my-2 md:mx-2 text-white flex flex-col items-center justify-center" >
@@ -64,18 +68,18 @@
             </div>
             
             <small class="vuecal__event-time text-white">
-                <span>{{ `${event.start.formatTime()}→${event.end.formatTime()}` }}</span>
+                <div class="flex flex-col md:flex-row justify-center items-center"><span>{{ event.start.formatTime() }}</span><span>→</span><span>{{ event.end.formatTime() }}</span></div>
             </small>
-
+            
             <div class="flex flex-col justify-end items-center h-auto">
                 <UTooltip class="m-2" v-if="event.organizer" :text="event.organizer.username" >
                     <UAvatar :alt="event.organizer.username" size="sm" />
                 </UTooltip>
                 <!-- <UButton v-if="event.class=='opening_hour'" @click="" variant="link">Réserver une table</UButton> -->
             </div>
-
+            
         </template>
-
+        
     </vue-cal>
     
 </UCard>
@@ -138,7 +142,10 @@ const eventRequest : Ref<EventEditionRequest | null> = ref(null)
         }
     })
     
+
+
     const onEventCreateStart = (event, deleteEvent) => {
+
         if (newCalEventObject.event == null) {
             newCalEventObject.event = event
         }
@@ -148,6 +155,9 @@ const eventRequest : Ref<EventEditionRequest | null> = ref(null)
         }
         return event
     }
+    
+
+    
     const onEventCreate = (event) => {
         
         eventRequest.value = {
@@ -212,7 +222,7 @@ const eventRequest : Ref<EventEditionRequest | null> = ref(null)
         if (index != -1) {
             eventsCal.value.splice(index, 1)
         }
-
+        
         showDetailsModal.value = false
     }
     
@@ -301,7 +311,7 @@ const eventRequest : Ref<EventEditionRequest | null> = ref(null)
                             found.start= new Date(data.start_at)
                             found.end = new Date(data.end_at)
                             found.organizer = data.organizer
-
+                            
                         }
                     }
                 }
@@ -380,6 +390,17 @@ const eventRequest : Ref<EventEditionRequest | null> = ref(null)
     @apply rounded-md text-white bg-teal-700;
 }
 
+.vuecal__cell--selected {
+    @apply bg-transparent;
+}
+
+.vuecal__cell--today {
+    @apply bg-transparent;
+}
+
+.vuecal__no-event {
+    @apply hidden
+}
 
 @media (max-width: 640px) {
     .vuecal__time-cell-label {
