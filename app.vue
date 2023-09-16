2252e7e29472a7a4dd076b0b9a6fcd222e861847
mick <mickheudre@gmail.com>
2023-09-16 16:25:50 +0200

    added cabin & basic metadata

--- app.vue
+++ app.vue
@@ -4,3 +4,26 @@
       <NuxtPage />
     </NuxtLayout>
   </template>
+
+  <script>
+  export default {
+    setup () {
+      useHead({
+        script: [
+        { 
+          async: true,
+          src: 'https://scripts.withcabin.com/hello.js',
+        }]
+      }) },
+      head: {
+        title: 'Tabletop Social Club'
+      }
+    }
+  </script>
+  
+  <style>
+  html, body {
+    height: 100%;
+    box-sizing: border-box;
+  }
+  </style>
